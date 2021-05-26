925a29ee4fd5112f0f5ba119de8dc3e8afc7aa63
martabil <77537664+martabil@users.noreply.github.com>
2021-05-26 20:48:50 +0200

    html upload

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,17 @@
+define(function() {var keywords=[{w:"Girls",p:["p0"]},{w:"on",p:["p0"]},{w:"longboards",p:["p0","p6"]},{w:"Longboard",p:["p1","p8","p9"]},{w:"events",p:["p1"]},{w:"and",p:["p1"]},{w:"tournaments",p:["p1"]},{w:"Longboarding",p:["p2"]},{w:"Most",p:["p3"]},{w:"popular",p:["p3"]},{w:"brands",p:["p3"]},{w:"Protective",p:["p4"]},{w:"gear",p:["p4"]},{w:"Styles_of_riding",p:["p5"]},{w:"Types",p:["p6"]},{w:"of",p:["p6"]},{w:"Longboard_vs_skateboard",p:["p7"]},{w:"How",p:["p8","p9"]},{w:"to",p:["p8","p9"]},{w:"choose",p:["p8"]},{w:"the",p:["p8"]},{w:"right",p:["p8"]},{w:"ride",p:["p9"]},{w:"a",p:["p9"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6];
+ph["p2"]=[7];
+ph["p3"]=[8, 9, 10];
+ph["p4"]=[11, 12];
+ph["p5"]=[13];
+ph["p6"]=[14, 15, 2];
+ph["p7"]=[16];
+ph["p8"]=[17, 18, 19, 20, 21, 3];
+ph["p9"]=[17, 18, 22, 23, 3];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
